2a67fbbab37ac3566a5ac3f9fa1239fc72f393fb
clarafontecoba <clara.fontecoba@etu.uca.fr>
2026-01-14 14:31:20 +0100

    Add files via upload

---
+++ Pousinade/index.php
@@ -0,0 +1,18 @@
+<?php
+    include ('configuration/config.php');
+    include ('scripts/actualite.php');
+    include ('scripts/contact.php');
+    include ('scripts/evenements.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ Pousinade/scripts/evenements.php
@@ -0,0 +1,17 @@
+<?php
+    include ('configuration/config.php');
+    include ('../index.php');
+    include ('scripts/contact.php');
+    include ('scripts/actualite.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nos évènements</title>
+</head>
+<body>
+    
+</body>
+</html>
