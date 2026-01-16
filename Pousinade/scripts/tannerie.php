5a8ac23c8a96ccd03652fd557337bb0a0cbc9dbe
liloualaidesvp <andrieuxlilou9@gmail.com>
2026-01-16 14:50:17 +0100

    modifs pages details decouvrir

--- Pousinade/scripts/tannerie.php
+++ Pousinade/scripts/tannerie.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles.css"> 
-    <title>Tannerie</title>
+    <title>Calligraphie</title>
+        <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
-    
+
+    <h1>Nos ateliers</h1>
+
+    <section class="atelier">
+        <h2>Calligraphie</h2>
+
+        <p>
+            Un travail personnel en dehors des cours sera nécessaire pour
+            évoluer dans cette pratique. Le matériel nécessaire durant les
+            cours sera mis à votre disposition.
+        </p>
+
+        <p>Débutant / intermédiaire</p>
+
+        <p>48€ + 5€ d’adhésion</p>
+
+        <p>Le jeudi de 17h30 à 20h30</p>
+
+        <p>Centre Pierre Cardinal – Le Puy en Velay</p>
+
+        <img src="../css/images/calligraphie.jpg" alt="Atelier calligraphie" width="400">
+    </section>
+
 </body>
 </html>
