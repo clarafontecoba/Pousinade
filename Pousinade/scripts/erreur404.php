4a105bce507fc463f7fdc3e682519e91d2306efd
liloualaidesvp <andrieuxlilou9@gmail.com>
2026-01-16 14:56:01 +0100

    ERREUR 404 PAGE

---
+++ Pousinade/scripts/erreur404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Erreur 404</title>
+    <link rel="stylesheet" href="css/404.css">
+</head>
+<body>
+
+    <section class="erreur">
+        <h2>ERREUR 404</h2>
+        <p>La page que vous recherchez n’existe pas.</p>
+
+        <a href="index.php">Retour à l’accueil</a>
+    </section>
+
+</body>
+</html>
